5b721545945a8491d6bde492a4c4cf14e8dd403b
mhieu2792006-creator <mhieu2792006@gmail.com>
2026-03-15 12:12:29 +0700

    Update BabyChickenModel.java

--- src/model/entity/egg/BabyChickenModel.java
+++ src/model/entity/egg/BabyChickenModel.java
@@ -4,5 +4,59 @@ package model.entity.egg;
  * HP: 1 | Nhỏ | Di chuyển nhanh ngang | Bắn lên player
  * ke thua tu ChickenModel
  */
+import model.entity.chicken.ChickenModel;
+
+public class BabyChickenModel extends ChickenModel {
+
+    private int hp = 1;
+
+    private double speedX = 4; // di chuyển nhanh ngang
+    private int direction = 1; // 1: phải, -1: trái
+
+    private int shootCooldown = 0;
+    private int shootDelay = 120; // thời gian giữa mỗi lần bắn
+
+    public BabyChickenModel(double x, double y) {
+        super(x, y);
+        this.width = 20;   // nhỏ
+        this.height = 20;
+    }
+
+    public void update() {
+
+        // di chuyển ngang
+        x += speedX * direction;
+
+        // đổi hướng khi chạm biên
+        if (x < 0 || x > 800) {
+            direction *= -1;
+        }
+
+        // xử lý bắn
+        shootCooldown++;
+
+        if (shootCooldown >= shootDelay) {
+            shoot();
+            shootCooldown = 0;
+        }
+    }
+
+    private void shoot() {
+        // bắn lên player (đạn hướng xuống player)
+        createBullet(x + width / 2, y + height);
+    }
+
+    public void takeDamage(int damage) {
+        hp -= damage;
+
+        if (hp <= 0) {
+            destroy();
+        }
+    }
+
+    public int getHp() {
+        return hp;
+    }
+}
 public class BabyChickenModel {
 }
